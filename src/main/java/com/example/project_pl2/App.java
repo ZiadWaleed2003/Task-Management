758369043ceb8c8e16fb9ef07d3610b748af970a
zyad atef <zyadatefali12@gmail.com>
2023-12-06 17:33:21 +0200

    ADDED A STYLESHEET FOR THE WHOLE PROGRAM

--- src/main/java/com/example/project_pl2/App.java
+++ src/main/java/com/example/project_pl2/App.java
@@ -2,24 +2,27 @@ package com.example.project_pl2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import java.io.*;
+import java.io.IOException;
 
-public class App extends Application {
+public class App extends Application{
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root =FXMLLoader.load(getClass().getResource("login.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
         Scene scene = new Scene(root);
-        stage.setTitle("Hello!");
+        stage.setTitle("WELCOME TO ELZOZAT TASK MANAGER");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
+
