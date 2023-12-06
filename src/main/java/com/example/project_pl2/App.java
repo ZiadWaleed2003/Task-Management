8b15c83f8537d5b948d31544bb66b64bfb38eb05
zyad atef <zyadatefali12@gmail.com>
2023-12-06 17:46:28 +0200

    err FIXED but commeneted waleed's part in app XD

--- src/main/java/com/example/project_pl2/App.java
+++ src/main/java/com/example/project_pl2/App.java
@@ -1,28 +1,60 @@
+//<<<<<<< HEAD
+
+package com.example.project_pl2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
+public class App extends Application{
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("WELCOME TO ELZOZAT TASK MANAGER");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+//=======
 //package com.example.project_pl2;
 //
 //import javafx.application.Application;
 //import javafx.fxml.FXMLLoader;
-//import javafx.stage.Stage;
 //import javafx.scene.Parent;
 //import javafx.scene.Scene;
+//import javafx.stage.Stage;
 //
-//import java.io.IOException;
+//import java.io.*;
+//import java.sql.*;
 //
-//public class App extends Application{
+//public class App extends Application {
 //    @Override
 //    public void start(Stage stage) throws IOException {
-//
-//        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+//        Parent root =FXMLLoader.load(getClass().getResource("login.fxml"));
 //        Scene scene = new Scene(root);
-//        stage.setTitle("WELCOME TO ELZOZAT TASK MANAGER");
+//        stage.setTitle("Hello!");
 //        stage.setResizable(false);
 //        stage.setScene(scene);
 //        stage.show();
-//
 //    }
 //
-//    public static void main(String[] args) {
-//        launch(args);
+//    public static void main(String[] args) throws SQLException {
+//
+//        Employee emp = new Employee();
+//        boolean x = emp.login("ZiadWaleed@gmail.com","2003");
 //    }
 //}
-//
+//>>>>>>> main
