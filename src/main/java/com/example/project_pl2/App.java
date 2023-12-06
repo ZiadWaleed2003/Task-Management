c15a9f220f75b69de81990d3bc98cbd37d0d402c
zyad atef <zyadatefali12@gmail.com>
2023-12-06 17:43:22 +0200

    commented APP becoz it was giving an err on merge

--- src/main/java/com/example/project_pl2/App.java
+++ src/main/java/com/example/project_pl2/App.java
@@ -1,28 +1,28 @@
-package com.example.project_pl2;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-
-import java.io.IOException;
-
-public class App extends Application{
-    @Override
-    public void start(Stage stage) throws IOException {
-
-        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
-        Scene scene = new Scene(root);
-        stage.setTitle("WELCOME TO ELZOZAT TASK MANAGER");
-        stage.setResizable(false);
-        stage.setScene(scene);
-        stage.show();
-
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
-
+//package com.example.project_pl2;
+//
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.stage.Stage;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//
+//import java.io.IOException;
+//
+//public class App extends Application{
+//    @Override
+//    public void start(Stage stage) throws IOException {
+//
+//        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+//        Scene scene = new Scene(root);
+//        stage.setTitle("WELCOME TO ELZOZAT TASK MANAGER");
+//        stage.setResizable(false);
+//        stage.setScene(scene);
+//        stage.show();
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
+//
