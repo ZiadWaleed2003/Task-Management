6fb22d97cea47ec14aaf7d4e6209d75bfc272f9a
AbuElzozz <ziad.adel3060@gmail.com>
2023-12-08 20:10:15 +0200

    Addel Mehtod add-update -delete project form project class

--- src/main/java/com/example/project_pl2/Utility.java
+++ src/main/java/com/example/project_pl2/Utility.java
@@ -6,8 +6,5 @@ public interface Utility {
      enum CompletionStatus {
         PENDING, INPROGRESS, COMPLETED, CANCELED
     }
-     static int generateID(int seed){
-        Random rand = new Random();
-        return seed * 100 + rand.nextInt(100);
-    }
+
 }
