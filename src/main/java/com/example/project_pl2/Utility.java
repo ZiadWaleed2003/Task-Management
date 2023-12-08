07b733b760e25804ea448cb84cffc8ced51c5364
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2023-12-08 23:30:13 +0200

    feat(util): add login singleton wrapper.

--- src/main/java/com/example/project_pl2/Utility.java
+++ src/main/java/com/example/project_pl2/Utility.java
@@ -1,10 +1,24 @@
 package com.example.project_pl2;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Random;
 
 public interface Utility {
      enum CompletionStatus {
         PENDING, IN_PROGRESS, COMPLETED, CANCELED
+    }
+    public class UserSingle{
+         private static UserSingle instance = null;
+         public Employee data = null;
+         private UserSingle(){}
+         public static UserSingle getInstance(){
+             if (instance == null){
+                 instance = new UserSingle();
+             }
+             return instance;
+         }
+
     }
 
 }
