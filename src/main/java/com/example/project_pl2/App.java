cce2a379c05dbf57af8e037e016288b4c3369a38
Ziad Waleed <ziadwaleedmohamed2003@gmail.com>
2023-12-09 23:09:55 +0200

    solving conflict

--- src/main/java/com/example/project_pl2/App.java
+++ src/main/java/com/example/project_pl2/App.java
@@ -44,9 +44,14 @@ public class App extends Application{
 //<<<<<<< HEAD
     public static void main(String[] args) {
         //launch();
-        Object[] arrgs = {5, "Test query for dynamic DML func", "Something"};
+        Object[] arrgs = {12, "Test query for dynamic DML func", "Something"};
         Pair<Boolean, Integer> res = CRUD2.updateDbDynamic("INSERT INTO request (Request_Id, Request_Desc," +
                 "Request_type) VALUES (?, ?, ?)",arrgs);
+        if(res.getKey())
+            System.out.print("result is : ");
+        else
+            System.out.print("Fasle");
+
     }
 
 //=======
