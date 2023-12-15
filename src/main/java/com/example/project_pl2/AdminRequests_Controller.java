b42f42771be669371134c05608df89cd608bdf0e
zyad atef <zyadatefali12@gmail.com>
2023-12-15 22:51:27 +0200

    added the data to table requests

--- src/main/java/com/example/project_pl2/AdminRequests_Controller.java
+++ src/main/java/com/example/project_pl2/AdminRequests_Controller.java
@@ -20,16 +20,56 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
-public class AdminRequests_Controller{
+public class AdminRequests_Controller implements  Initializable{
     @FXML
     private Button LogoutButton;
+    ObservableList<Request> requestObservableList = FXCollections.observableArrayList();
+
+    @FXML
+    private TableView<Request>  requestTableView;
+    @FXML
+    private TableColumn<Request,Integer> ID;
+    @FXML
+    private TableColumn<Request,String> Type;
+    @FXML
+    private TableColumn<Request,String> request_desc;
+    @FXML
+    private TableColumn<Request,Boolean> status;
 
     private Scene scene;
     private Parent root;
     private Stage stage;
-    @FXML
-    private Label Selection;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            ArrayList<Request> requests = Utility.UserSingle.getInstance().admin.reviewRequests();
+
+            requestObservableList.addAll(requests);
+
+            ID.setCellValueFactory(new PropertyValueFactory<Request,Integer>("request_id"));
+            Type.setCellValueFactory(new PropertyValueFactory<Request,String>("request_type"));
+            request_desc.setCellValueFactory(new PropertyValueFactory<Request,String>("request_description"));
+            status.setCellValueFactory(new PropertyValueFactory<Request,Boolean>("request_status"));
+
+            requestTableView.setItems(requestObservableList);
+
+
+        } catch (Exception e){
+            System.out.println("A7A");
+        }
+
+
+
+    }
+
+
+
+
 
 
             public void Logout(ActionEvent event)throws IOException{
@@ -66,6 +106,7 @@ public class AdminRequests_Controller{
                 stage.show();
 
             }
+
 
 }
 
